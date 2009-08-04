9d1d071cfe3114b25a9b9bbd05d22c4acfcf3645
Tim Horton <hortont@svn.gnome.org>
2009-08-04 04:42:59 -0400

    Generate basic schedules

--- DisplayPage.py
+++ DisplayPage.py
@@ -1,39 +1,121 @@
+# -*- coding: utf-8 -*-
+
 import datetime
+import db
+
+resid = db.getResourceId("Lab Computers")
+
+def generateDaySlots(weekOf, startTime):
+    daySlots = ""
+    day = datetime.datetime.combine(weekOf, startTime)
+    
+    for i in range(0, 5):
+        entries = db.getEntries(day.date(), startTime, resid)
+        quantity = db.getResourceQuantity(resid)
+        
+        if entries:
+            daySlots += "<td class='tFilled'>"
+        else:
+            daySlots += "<td>"
+        
+        for entry in entries:
+            daySlots += "<div class='entry'>"
+            daySlots += db.getTeacher(entry[7])
+            daySlots += " (%(q)d)" % {"q": entry[5]}
+            daySlots += "</div>"
+            quantity -= entry[5]
+        
+        if entries:
+            daySlots += "<div class='leftEntryAfter'>%(q)d left</div>" % {"q": quantity}
+        
+        daySlots += "</td>"
+        
+        day = day + datetime.timedelta(days=1)
+    
+    return daySlots
 
 def generateComputersPage(weekOf):
-    return weekOf.isoformat()
+    nextWeek = lastWeek = datetime.datetime.combine(weekOf, datetime.time(0,0))
+    
+    nextWeek = nextWeek + datetime.timedelta(weeks=1)
+    nextWeekStr = nextWeek.strftime("%Y-%m-%d")
+    
+    lastWeek = lastWeek - datetime.timedelta(weeks=1)
+    lastWeekStr = lastWeek.strftime("%Y-%m-%d")
+    
+    yield u"""
+    <div id="schedule">
+        <div id="scheduleHeader">%(last)s Schedule for week of %(week)s %(next)s</div>
+        <table id="scheduleTable" cellpadding="0px" cellspacing="4px">
+            <tr>
+                <td class="tHide"></td>
+                <td class="tHeader">Monday</td>
+                <td class="tHeader">Tuesday</td>
+                <td class="tHeader">Wednesday</td>
+                <td class="tHeader">Thursday</td>
+                <td class="tHeader">Friday</td>
+            </tr>""" % {"week": weekOf.strftime("%B %d, %Y"),
+                        "last": "<a href='?date=" + lastWeekStr + u"'>←</a>",
+                        "next": "<a href='?date=" + nextWeekStr + u"'>→</a>"}
+    
+    startTime = datetime.datetime.combine(weekOf, datetime.time(8,45))
+    endTime = startTime + datetime.timedelta(minutes=35)
+    
+    for i in range(0,9):
+        yield """
+        <tr>
+            <td class="tTime">%(startTime)s - %(endTime)s</td>
+            %(daySlots)s
+        </tr>
+        """ % {"startTime": startTime.strftime("%I:%M"),
+               "endTime": endTime.strftime("%I:%M"),
+               "daySlots": generateDaySlots(weekOf, startTime.time())}
+        
+        startTime = startTime + datetime.timedelta(minutes=40)
+        endTime = endTime + datetime.timedelta(minutes=40)
 
 class DisplayPage:
     def computers(self, date=None):
         yield """
         <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+            "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
         <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
         <head>
-        	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-        	<title>MBS Technology Signout</title>
-        	<link rel="stylesheet" href="/static/style.css" type="text/css" charset="utf-8" />
+            <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+            <title>MBS Technology Signout</title>
+            <link rel="stylesheet" href="/static/style.css" type="text/css" charset="utf-8" />
         </head>
         <body id="tab1">
-        	<div id="header">
-        		<a href="/"><img src="/static/signout-logo.png" id="logo"/></a>
-        		<ul id="tabnav">
-        			<li class="tab1"><a href="/computers">Lab Computers</a></li>
-        			<li class="tab2"><a href="/laptops">Laptops</a></li>
-        			<li class="tab3"><a href="/projectors">Projectors</a></li>
-        		</ul>
-        	</div>
-        	<a href="signout_computers.html"><div id="signoutButton">
-        		<img src="/static/add.tiff" valign="top"/> Sign out computers
-        	</div></a>"""
+            <div id="header">
+                <a href="/"><img src="/static/signout-logo.png" id="logo"/></a>
+                <ul id="tabnav">
+                    <li class="tab1"><a href="/computers">Lab Computers</a></li>
+                    <li class="tab2"><a href="/laptops">Laptops</a></li>
+                    <li class="tab3"><a href="/projectors">Projectors</a></li>
+                </ul>
+            </div>
+            <a href="signout_computers.html"><div id="signoutButton">
+                <img src="/static/add.tiff" valign="top"/> Sign out computers
+            </div></a>
+            <div id="schedule">"""
         
         if date is None:
-            date = datetime.date(2009,9,4)
+            date = datetime.date.today()
+        else:
+            try:
+                date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+            except:
+                date = datetime.date.today()
         
-        yield generateComputersPage(date)
+        if date.weekday():
+            tmpdate = datetime.datetime.combine(date, datetime.time(0,0))
+            tmpdate = tmpdate - datetime.timedelta(days=date.weekday())
+            date = tmpdate
+        
+        yield "".join(list(generateComputersPage(date)))
         
         yield """
-        	</div>
+            </div>
         </body>
         </html>"""
 
--- db.py
+++ db.py
@@ -1,11 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import datetime
 import sqlite3
 from utils import *
 
+def getEntries(day,time,type):
+    c = connect()
+    c.execute("select * from entries where day=? and start_time=? and signee_id=?",
+              (day.strftime("%Y-%m-%d"), time.strftime("%I:%M"), type))
+    return c.fetchall()
+
+def getTeacher(t_id):
+    c = connect()
+    c.execute("select name from teachers where id=?", (t_id,))
+    return c.fetchone()[0]
+
+def getResourceId(res):
+    c = connect()
+    c.execute("select id from resources where name=?", (res,))
+    return c.fetchone()[0]
+
+def getResourceQuantity(r_id):
+    c = connect()
+    c.execute("select quantity from resources where id=?", (r_id,))
+    return c.fetchone()[0]
+
 def loadData(c):
     c.executescript(readFile("sql.txt"))
-    c.execute("insert into resources values (?,?,?,?)", (None,"Computers","",25))
-    c.execute("insert into resources values (?,?,?,?)", (None,"Laptops","",20))
-    c.execute("insert into resources values (?,?,?,?)", (None,"Projectors","",2))
+    c.execute("insert into resources values (?,?,?,?)", (1,"Lab Computers","",25))
+    c.execute("insert into resources values (?,?,?,?)", (2,"Laptops","",20))
+    c.execute("insert into resources values (?,?,?,?)", (3,"Projectors","",2))
     
     c.execute("insert into teachers values (?,?,?)", (None,"Eaton",""))
     c.execute("insert into teachers values (?,?,?)", (None,"Cheney",""))
@@ -14,13 +38,24 @@ def loadData(c):
     c.execute("insert into teachers values (?,?,?)", (None,"Kilmer",""))
     c.execute("insert into teachers values (?,?,?)", (None,"Fitzpatrick",""))
     
-    c.execute('select id from resources where name=?', ("Computers",))
-    c_id = c.fetchone()[0]
+    c_id = 1
     c.execute('select id from teachers where name=?', ("Cheney",))
-    e_id = c.fetchone()[0]
+    cheney_id = c.fetchone()[0]
+    c.execute('select id from teachers where name=?', ("Eaton",))
+    eaton_id = c.fetchone()[0]
     c.execute("insert into entries values (?,?,?,?,?,?,?,?)",
-        (None, "2009-02-05", "8:45", "9:20", "", 5, c_id, e_id))
+        (None, "2009-08-05", "08:45", "09:20", "", 5, c_id, eaton_id))
+    c.execute("insert into entries values (?,?,?,?,?,?,?,?)",
+        (None, "2009-08-06", "08:45", "09:20", "", 5, c_id, eaton_id))
+    c.execute("insert into entries values (?,?,?,?,?,?,?,?)",
+        (None, "2009-08-07", "08:45", "09:20", "", 5, c_id, eaton_id))
+    c.execute("insert into entries values (?,?,?,?,?,?,?,?)",
+        (None, "2009-08-05", "09:25", "10:00", "", 15, c_id, cheney_id))
+    c.execute("insert into entries values (?,?,?,?,?,?,?,?)",
+        (None, "2009-08-05", "09:25", "10:00", "", 5, c_id, eaton_id))
 
-db = sqlite3.connect(':memory:')
-c = db.cursor()
-loadData(c)
+def connect():
+    db = sqlite3.connect(':memory:')
+    c = db.cursor()
+    loadData(c)
+    return c
